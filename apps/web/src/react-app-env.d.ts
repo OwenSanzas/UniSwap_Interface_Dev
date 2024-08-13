7953630ee3f071c320c33495206bb3090c7ea5fd
Ze Sheng <osanzas1997@gmail.com>
2024-08-13 04:18:17 -0500

    CICD test

--- apps/web/src/react-app-env.d.ts
+++ apps/web/src/react-app-env.d.ts
@@ -21,6 +21,8 @@ declare module 'content-hash' {
   declare function getCodec(x: string): string
 }
 
+
+// test
 declare module 'multihashes' {
   declare function decode(buff: Uint8Array): { code: number; name: string; length: number; digest: Uint8Array }
   declare function toB58String(hash: Uint8Array): string
